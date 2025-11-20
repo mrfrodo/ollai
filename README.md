b84fe06b6f7ac98d6f422fce1f7612ca51f0d6c6
Frodo B <frodo@mordor.com>
2025-11-20 13:49:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Updated for spring boot 3.5.7
+
+# Updated for spring boot ai 1.1.0
---
+++ src/main/java/com/example/ollai/OllamaApp.java
@@ -0,0 +1,13 @@
+package com.example.ollai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaApp.class, args);
+	}
+
+}
